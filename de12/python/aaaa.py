9f5a0de976615dbf5dd644550e8cd0da4f3a2dad
soukenbicya <r202202233ck@jindai.jp>
2022-10-28 15:22:24 +0900

    20221028

--- de12/python/aaaa.py
+++ de12/python/aaaa.py
@@ -3,73 +3,72 @@ value = int(input('直感で2回数字を選び、1～5の数を入力してく�
 if value == 1:
         value = int(input('直感で数字を選び、1～5の数を入力してください'))
         if value == 1:
-         print('３です')
+         print('あなたのおすすめの素数は２１５３です')
         elif value == 2:
-         print('５です')
+         print('あなたにおすすめの素数は４０１です')
         elif value == 3:
-         print('７です')
+         print('あなたにおすすめの素数は１２３です')
         elif value == 4:
-         print('１１です')
+         print('あなたにおすすめ素数は２４４１です')
         elif value == 5:
-         print('１２３です')
+         print('あなたにおすすめ素数は３５３３です')
         else:
          print('やり直してください')
 elif value == 2:
         value = int(input('直感で数字を選び、1～5の数を入力してください'))
         if value == 1:
-         print('３です')
+         print('あなたにおすすめ素数は３です')
         elif value == 2:
-         print('５です')
+         print('あなたにおすすめ素数は５です')
         elif value == 3:
-         print('７です')
+         print('あなたにおすすめ素数は７です')
         elif value == 4:
-         print('１１です')
+         print('あなたにおすすめ素数は１１です')
         elif value == 5:
-         print('１２３です')
+         print('あなたにおすすめ素数は１９です')
         else:
          print('やり直してください')
 elif value == 3:
         value = int(input('直感で数字を選び、1～5の数を入力してください'))
         if value == 1:
-         print('３です')
+         print('あなたにおすすめ素数は３８４７です')
         elif value == 2:
-         print('５です')
+         print('あなたにおすすめ素数は６５５３です')
         elif value == 3:
-         print('７です')
+         print('あなたにおすすめ素数は７０１９です')
         elif value == 4:
-         print('１１です')
+         print('あなたにおすすめ素数は８１０１です')
         elif value == 5:
-         print('１２３です')
+         print('あなたにおすすめ素数は９９４９です')
         else:
          print('やり直してください')
 elif value == 4:
         value = int(input('直感で数字を選び、1～5の数を入力してください'))
         if value == 1:
-         print('３です')
+         print('あなたにおすすめ素数は９９３１です')
         elif value == 2:
-         print('５です')
+         print('あなたにおすすめ素数は７４３３です')
         elif value == 3:
-         print('７です')
+         print('あなたにおすすめ素数は６８２７です')
         elif value == 4:
-         print('１１です')
+         print('あなたにおすすめ素数は８７７です')
         elif value == 5:
-         print('１２３です')
+         print('あなたにおすすめ素数は１７８３です')
         else:
          print('やり直してください')
 elif value == 5:
         value = int(input('直感で数字を選び、1～5の数を入力してください'))
         if value == 1:
-         print('３です')
+         print('あなたにおすすめ素数は４９０３です')
         elif value == 2:
-         print('５です')
+         print('あなたにおすすめ素数は４４４１です')
         elif value == 3:
-         print('７です')
+         print('あなたにおすすめ素数は２です')
         elif value == 4:
-         print('１１です')
+         print('あなたにおすすめ素数は４１です')
         elif value == 5:
-         print('１２３です')
+         print('あなたにおすすめ素数は７３です')
         else:
          print('やり直してください')
 else:
     print('やり直してください')
-     
